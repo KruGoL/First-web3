dcff47038ced9b44757d75950fd33550b3c0a4fa
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2023-06-05 16:26:07 +0300

    TransactionContext 1:57

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,11 +21,18 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const {value} = useContext(TransactionContext);
-  console.log(value);
-  const connectWallet = () => {};
+  const { connectWallet, currentAccount, formData, sendTransaction, handleChange } =
+    useContext(TransactionContext);
 
-  const handleSubmit = () => {};
+  const handleSubmit = () => {
+    const {addressTo, amount, keyword, message} = formData;
+
+    e.preventDefault();
+
+    if(!addressTo ||!amount ||!keyword ||!message) return ;
+
+    sendTransaction();
+  };
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -38,13 +45,17 @@ const Welcome = () => {
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className=" flex flex-row justify-center items-center cursor-pointer my-5 bg-[#2952e3] p-3 rounded-full hover:bg-[#2546bd]"
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className=" flex flex-row justify-center items-center cursor-pointer my-5 bg-[#2952e3] p-3 rounded-full hover:bg-[#2546bd]"
+            >
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={` rounded-tl-2xl ${commonStyles}`}>Reliability</div>
@@ -79,25 +90,25 @@ const Welcome = () => {
               placeholder="Address To"
               name="addressTo"
               type="text"
-              handleChange={() => {}}
+              handleChange={() => {handleChange()}}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={() => {handleChange()}}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={() => {handleChange()}}
             />
             <Input
               placeholder="Enter Message"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={() => {handleChange()}}
             />
 
             <div className="h-[1px] w-full bg-gray-400 my-2"></div>
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -20,7 +20,84 @@ const getEthereumContract = () => {
 };
 
 export const TransactionProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) {
+        return alert("Please install metamask");
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+        //getAllTransactions();
+      } else {
+        console.log("No account found");
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error("No etherium object.");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) {
+        return alert("Please install metamask");
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+      throw new Error("No etherium object.");
+    }
+  };
+
+  const sendTransaction = async (to, value) => {
+    try {
+      if (!ethereum) {
+        return alert("Please install metamask");
+      }
+
+      //get the data from the form...
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No etherium object.");
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
-    <TransactionContext.Provider value={{ value : 'test'}}> {children} </TransactionContext.Provider>
+    <TransactionContext.Provider
+      value={{
+        connectWallet,
+        currentAccount,
+        formData,
+        setFormData,
+        handleChange,
+        sendTransaction,
+      }}
+    >
+      {children}
+    </TransactionContext.Provider>
   );
 };
